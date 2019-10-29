2d52ce6a5260cba0cb9f47d9ff160aaa4931246b
Amol Kelkar <kelkar.amol@gmail.com>
2019-10-28 23:30:17 -0700

    Prep for Spider submission

---
+++ predict.sh
@@ -0,0 +1,6 @@
+allennlp predict trained_model/ $1 --predictor spider_discriminator --use-dataset-reader --cuda-device=-1 --silent --output-file beam_predictions.jsonlines --include-package models.semantic_parsing.spider_parser --include-package dataset_readers.spider --include-package predictors.discriminator_dataset_generator --weights-file trained_model/model.th -o "{\"dataset_reader\":{\"keep_if_unparsable\":true, \"dataset_path\": \"$1/database\", \"tables_file\":\"$1/tables.json\"}}"
+
+python3 rerank_beam_outputs.py --input-file beam_predictions.jsonlines --output-file $2 --model trained_model/reranker.th --sqlonly
+
+# Evaluate
+PYTHONPATH=. python3 semparse/worlds/evaluate.py --gold dataset/dev_gold.sql --pred predictions.sql --db dataset/database/ --table dataset/tables.json --etype match
